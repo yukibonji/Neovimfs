0819c2b8e31dd752491247c5ffc47a6637736052
callmekohei <callmekohei@gmail.com>
2017-03-04 05:54:25 +0900

    toriaezu

--- neovimfs.fsx
+++ neovimfs.fsx
@@ -11,6 +11,15 @@ open System.Text.RegularExpressions
 open System.Collections.Generic
 open System.Collections.Concurrent
 
+#r @"./packages/FSharp.Control.Reactive/lib/net45/FSharp.Control.Reactive.dll"
+#r @"./packages/Rx-Core/lib/net45/System.Reactive.Core.dll"
+#r @"./packages/Rx-Linq/lib/net45/System.Reactive.Linq.dll"
+#r @"./packages/Rx-Interfaces/lib/net45/System.Reactive.Interfaces.dll"
+#r @"./packages/Rx-PlatformServices/lib/net45/System.Reactive.PlatformServices.dll"
+open FSharp.Control.Reactive
+open FSharp.Control.Reactive.Observable
+open System.Reactive.Linq
+
 #r @"./packages/FSharp.Compiler.Service/lib/net45/FSharp.Compiler.Service.dll"
 open Microsoft.FSharp.Compiler
 open Microsoft.FSharp.Compiler.SourceCodeServices
@@ -101,13 +110,76 @@ module private FsharpInteractive =
                 |> Array.distinct
                 |> Array.iter ( fun s -> stdout.WriteLine(s) )
 
+                
 
-module private FSharpIntellisence  =
+(*----------------------------------------------------------------------------------
 
-    type PostData   = { Row:string; Col:string; Line:string; FilePath:string; Source:string }
-    type JsonFormat = { word : string; info: string list list  }
+                              intellisense
 
-    let private asyncGetDeclarationListInfo ( checker:FSharpChecker) ( ps:PostData ) (arr:(string array * string)) : Async<FSharpDeclarationListInfo> = async {
+-----------------------------------------------------------------------------------*)
+
+
+type PostData   = { Row:string; Col:string; Line:string; FilePath:string; Source:string }
+
+type JsonFormat = { word : string; info: string list list  }
+
+type SetOperationEventArgs<'a>(postData:PostData) =
+    inherit System.EventArgs()
+    member this.PS = postData 
+
+type dlg = delegate of obj * SetOperationEventArgs<PostData> -> unit
+
+type Generator() =
+    let    m_event = new Event<dlg, SetOperationEventArgs<PostData>>()
+    member this.Evnt      = m_event.Publish
+    member this.OnEvnt (x) = m_event.Trigger(this, new SetOperationEventArgs<_>(x))
+
+
+module Util =
+
+    let angleBracket ( s:string ) :string =
+        s.Split([|'<';'>'|])
+        |> Array.filter ( fun s -> s <> "" )
+        |> fun arr ->
+           if     Array.last arr = "."
+           then   Array.head arr
+           else   (Array.tail arr).[0]
+
+    let nameSpaceArrayImpl ( s:string) :string array =
+        s.Split('.')
+        |> Array.filter ( fun s -> s <> "" )
+        |> fun arr ->
+            if    Array.last arr = ""
+            then  Array.splitAt (arr.Length - 1) arr |> fst
+            else  arr
+
+    let nameSpaceArray (s:string) : string array =
+        s
+        |> fun s -> s.Split(' ')
+        |> Array.filter ( fun s -> s <> "" )
+        |> Array.last
+        |> fun ( s:string) ->
+            if    s.Contains("<")
+            then  angleBracket s |> nameSpaceArrayImpl
+            elif  s.Contains(".")
+            then  nameSpaceArrayImpl s
+            else  [|s|]
+        |> Array.map( fun s -> Regex.Replace(s,"\(.*\)","") )
+
+    let extractJson ctx key =
+        match ctx.request.formData key with
+        | Choice1Of2 x -> x
+        | _            -> ""
+    
+
+    let extractGroupTexts = function
+        | FSharpToolTipElement.None                    -> []
+        | FSharpToolTipElement.Single (a,b)            -> [a]
+        | FSharpToolTipElement.SingleParameter (a,b,c) -> []
+        | FSharpToolTipElement.Group xs                -> xs |> List.map fst
+        | FSharpToolTipElement.CompositionError s      -> [s]
+
+    let asyncGetDeclarationListInfo ( checker:FSharpChecker) ( ps:PostData ) (arr:(string array * string)) : Async<FSharpDeclarationListInfo> = async {
 
         let! (projOptions : FSharpProjectOptions) =
             checker.GetProjectOptionsFromScript( ps.FilePath , ps.Source )
@@ -124,87 +196,57 @@ module private FSharpIntellisence  =
         return! checkFileResults.GetDeclarationListInfo ( Some parseFileResults, int(ps.Row) , int(ps.Col) , ps.Line, (fst arr) |> Array.toList, (snd arr), fun _ -> false )
     }
 
-    let private angleBracket ( s:string ) :string =
-        s.Split([|'<';'>'|])
-        |> Array.filter ( fun s -> s <> "" )
-        |> fun arr ->
-           if     Array.last arr = "."
-           then   Array.head arr
-           else   (Array.tail arr).[0]
 
-    let private nameSpaceArrayImpl ( s:string) :string array =
-        s.Split('.')
-        |> Array.filter ( fun s -> s <> "" )
-        |> fun arr ->
-            if    Array.last arr = ""
-            then  Array.splitAt (arr.Length - 1) arr |> fst
-            else  arr
+module  FSharpIntellisence  =
+    open Util
 
-    let private nameSpaceArray (s:string) : string array =
-        s
-        |> fun s -> s.Split(' ')
-        |> Array.filter ( fun s -> s <> "" )
-        |> Array.last
-        |> fun ( s:string) ->
-            if    s.Contains("<")
-            then  angleBracket s |> nameSpaceArrayImpl
-            elif  s.Contains(".")
-            then  nameSpaceArrayImpl s
-            else  [|s|]
-        |> Array.map( fun s -> Regex.Replace(s,"\(.*\)","") )
+    let jsonSerializer:JsonSerializer = FsPickler.CreateJsonSerializer(indent = false, omitHeader = true) 
 
-    let public intellisense (fsc:FSharpChecker) (ctx: HttpContext) ( dic : ConcurrentDictionary<string,string> ) : string =
+    let jsonStrings (fsc:FSharpChecker) (postData:PostData) (nameSpace: string [] )  (word:string) : string =
+        asyncGetDeclarationListInfo fsc postData ( nameSpace, word ) |> Async.RunSynchronously |> fun x -> x.Items
+        |> Array.fold ( fun state x ->
+            let dt : JsonFormat = { word = x.Name; info = match x.DescriptionText with FSharpToolTipText xs -> List.map extractGroupTexts xs }
+            state + "\n" + jsonSerializer.PickleToString( dt ) ) ""
 
-        let extractJson key =
-            match ctx.request.formData key with
-            | Choice1Of2 x -> x
-            | _            -> ""
+    let asyncInit (fsc:FSharpChecker) (dic:ConcurrentDictionary<string,string>) (postData:PostData) : Async<unit> = async {
+        dic.GetOrAdd( "filePath" , postData.FilePath ) |> ignore 
 
-        let extractGroupTexts = function
-            | FSharpToolTipElement.None                    -> []
-            | FSharpToolTipElement.Single (a,b)            -> [a]
-            | FSharpToolTipElement.SingleParameter (a,b,c) -> []
-            | FSharpToolTipElement.Group xs                -> xs |> List.map fst
-            | FSharpToolTipElement.CompositionError s      -> [s]
+        // Do not use Array.Parallel.iter because too late !
+        [|"System" ; "List" ; "Set" ; "Seq" ; "Array" ; "Map" ; "Option" |]
+        |> Array.iter ( fun (s:string) -> dic.GetOrAdd ( s, jsonStrings fsc postData [|s|] "" ) |> ignore )
+
+        dic.GetOrAdd( "OneWordHint" , jsonStrings fsc postData [||] "" ) |> ignore 
+    }
+
+    let asyncReInit (fsc:FSharpChecker) (dic:ConcurrentDictionary<string,string>) (postData:PostData) : Async<unit> = async {
+        dic.TryUpdate( "filePath"   , postData.FilePath                        , dic.Item("filePath")    ) |> ignore
+        dic.TryUpdate( "System"     , jsonStrings fsc postData [|"System"|] "" , dic.Item("System")      ) |> ignore 
+        dic.TryUpdate( "OneWordHint", jsonStrings fsc postData [||] ""         , dic.Item("OneWordHint") ) |> ignore 
+    }
+
+    let asyncReOneWordHint (fsc:FSharpChecker) (dic:ConcurrentDictionary<string,string>) (postData:PostData) : Async<unit> = async {
+        
+        // dic.TryUpdate( "OneWordHint", jsonStrings fsc postData [||] ""         , dic.Item("OneWordHint") ) |> ignore 
+
+        let x = jsonStrings fsc postData [||] ""
+
+        dic.AddOrUpdate( "OneWordHint", x , fun key existingVal ->
+            if      x <> existingVal
+            then    x
+            else    dic.Item("OneWordHint")
+        ) |> ignore
+
+    }
+
+    let public intellisense (fsc:FSharpChecker) (ctx: HttpContext) ( dic : ConcurrentDictionary<string,string> ) ( gen : Generator ) : string =
  
-        let postData:PostData = { Row = extractJson "row"; Col = extractJson "col"; Line = extractJson "line"; FilePath = extractJson "filePath"; Source = extractJson "source" }
+        let postData:PostData = {
+            Row      = extractJson ctx "row"
+            Col      = extractJson ctx "col"
+            Line     = extractJson ctx "line"
+            FilePath = extractJson ctx "filePath"
+            Source   = extractJson ctx "source" }
 
-        let jsonSerializer:JsonSerializer = FsPickler.CreateJsonSerializer(indent = false, omitHeader = true) 
-
-        let jsonStrings (nameSpace: string [] )  (word:string) : string =
-            asyncGetDeclarationListInfo fsc postData ( nameSpace, word ) |> Async.RunSynchronously |> fun x -> x.Items
-            |> Array.fold ( fun state x ->
-                let dt : JsonFormat = { word = x.Name; info = match x.DescriptionText with FSharpToolTipText xs -> List.map extractGroupTexts xs }
-                state + "\n" + jsonSerializer.PickleToString( dt ) ) ""
-
-        let asyncInit () : Async<unit> = async {
-            dic.GetOrAdd( "filePath" , postData.FilePath ) |> ignore 
-
-            // Do not use Array.Parallel.iter because too late !
-            [|"System" ; "List" ; "Set" ; "Seq" ; "Array" ; "Map" ; "Option" |]
-            |> Array.iter ( fun s -> dic.GetOrAdd ( s, jsonStrings [|s|] "" ) |> ignore )
-
-            dic.GetOrAdd( "OneWordHint" , jsonStrings [||] "" ) |> ignore 
-        }
-
-        let asyncReInit () : Async<unit> = async {
-            dic.TryUpdate( "filePath"   , postData.FilePath           , dic.Item("filePath")    ) |> ignore
-            dic.TryUpdate( "System"     , jsonStrings [|"System"|] "" , dic.Item("System")      ) |> ignore 
-            dic.TryUpdate( "OneWordHint", jsonStrings [||] ""         , dic.Item("OneWordHint") ) |> ignore 
-        }
-
-        let asyncReOneWordHint () : Async<unit> = async {
-            
-            let x = jsonStrings [||] ""
-           
-           dic.AddOrUpdate( "OneWordHint", x , fun key existingVal ->
-                if      x <> existingVal
-                then    x
-                else    dic.Item("OneWordHint")
-            ) |> ignore
-
-        }
- 
         let dotHint () : string =
             let arr = nameSpaceArray postData.Line
             match Array.last arr with
@@ -234,25 +276,29 @@ module private FSharpIntellisence  =
             with
                 | :? System.ArgumentException -> ""
 
+        let abc (): string =
+            postData.Line.Split(' ')
+            |> Array.filter ( fun s -> s <> "" )
+            |> fun ary ->
+               if    Array.contains "[<" ary  && not ( Array.contains ">]" ary )
+               then  attributeHint ( Array.last ary |> fun s -> s.Replace( "[<","" ) )
+               else  oneWordHint   ( Array.last ary )
+
 
         if      Seq.isEmpty dic.Keys
-        then    asyncInit ()   |> Async.RunSynchronously
+        then    asyncInit fsc dic postData   |> Async.RunSynchronously
         elif    dic.Item( "filePath" ) <> postData.FilePath
-        then    asyncReInit () |> Async.Start 
+        then    asyncReInit fsc dic postData |> Async.Start 
 
         if      postData.Line.Contains(".")
-        then    dotHint ()
-        else    
-                if      System.Random().Next(5) = 4 
-                then    asyncReOneWordHint () |> Async.Start
-                else    ()
+        then    dotHint    ()
+        else
+                
+                gen.OnEvnt postData
+                abc ()
 
-                postData.Line.Split(' ')
-                |> Array.filter ( fun s -> s <> "" )
-                |> fun ary ->
-                   if    Array.contains "[<" ary  && not ( Array.contains ">]" ary )
-                   then  attributeHint ( Array.last ary |> fun s -> s.Replace( "[<","" ) )
-                   else  oneWordHint   ( Array.last ary )
+        
+
 
 
 module private Suave =
@@ -266,7 +312,6 @@ module private Suave =
         GET >=> pathScan "/evalScript/%s" ( fun fp ->
 
             // switch stdout to memory stream
-            // because fsiSession.EvalScriptNonThrowing's normal output to stdout
             use ms = new MemoryStream()
             use sw = new StreamWriter(ms)
             use tw = TextWriter.Synchronized(sw)
@@ -281,10 +326,11 @@ module private Suave =
 
             OK ( sr.ReadToEnd() ) )
 
-    let private autoComplete (fsc:FSharpChecker) ( dic :ConcurrentDictionary<string,string> ) =
+    let private autoComplete (fsc:FSharpChecker) ( dic :ConcurrentDictionary<string,string> ) ( gen : Generator ) =
 
         POST >=> path "/autoComplete" >=> ( fun (ctx: HttpContext) ->
-            OK (intellisense fsc ctx dic ) ctx )
+            OK (intellisense fsc ctx dic gen ) ctx )
+
 
 
     let private app (fsiPath:string) =
@@ -292,9 +338,15 @@ module private Suave =
         let fsi = Fsi(fsiPath)
         let fsc = FSharpChecker.Create()
         let dic : ConcurrentDictionary<string,string> = new ConcurrentDictionary< string, string >()
+        let gen = new Generator()
+
+        gen.Evnt
+        |> Observable.throttle  ( System.TimeSpan.FromMilliseconds(2000.))
+        |> Observable.subscribe ( fun e -> asyncReOneWordHint fsc dic e.PS |> Async.Start )
+        |> ignore 
 
         choose [ evalScript    fsi
-                 autoComplete  fsc dic
+                 autoComplete  fsc dic gen
                  NOT_FOUND     "Resource not found." ]
 
 
